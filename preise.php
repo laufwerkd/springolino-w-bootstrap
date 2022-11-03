65f8e7cad69f9a733b6847ee414f7e78fb87c8cd
H0unt <fabian.weber@edu.bib.de>
2022-11-03 10:43:49 +0100

    Preise und Zeiten Seiten

--- preise.php
+++ preise.php
@@ -1,7 +1,66 @@
 <?php include 'header.php'; ?>
 
-<main>
-    <h1>Preisliste</h1>
+<main class="mt-4">
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                Tageseintritt
+            </th>
+        </thead>
+        <tbody class="px-5">
+            <tr>
+                <td class="px-5 col-6">Kinder ab 3 Jahre</td>
+                <td class="px-5 col-6">11,50 € (unter 3 Jahre frei)</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Erwachsene ab 18 Jahre</td>
+                <td class="px-5 col-6">5,50 €</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Kinder mit Behindertenausweis</td>
+                <td class="px-5 col-6">6,00 € (unter 3 Jahre frei)</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Erwachsene mit Behindertenausweis</td>
+                <td class="px-5 col-6">3,00 € (unter 3 Jahre frei)</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                10er Karte
+            </th>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="px-5 col-6">10er Kinder</td>
+                <td class="px-5 col-6">99,00 €</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">10er Erwachsene</td>
+                <td class="px-5 col-6">49,00 €</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                 <br>
+            </th>
+        </thead>
+    </table>
+
+    <p class="mx-5">
+        <b>*Bei uns ist das Mitbringen von eigenen Speisen und Getränken nicht gestattet!
+        Wir bitten um Ihr Verständnis.</b> *Kinder unter 12 Jahren haben nur in Begleitung
+        eines Erwachsenen Zutritt. *Kinder über 12 Jahren haben gegen Vorlage eines
+        gültigen Personalausweises und nur mit Unterschrift eines Erziehungsberechtigten
+        Zutritt. Hier können Sie unsere Einverständniserklärung downloaden Falls Sie Interesse
+        an einer Gruppenermäßigung haben, können Sie sich telefonisch unter 0 52 21 – 69 24 24 melden.
+    </p>
 </main>
 
 <?php include 'footer.php'; ?>
--- zeiten.php
+++ zeiten.php
@@ -1,7 +1,133 @@
 <?php include 'header.php'; ?>
 
-<main>
-    <h1>Öffnungszeiten</h1>
+<main class="mt-4">
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                Generelle Öffnungszeiten
+            </th>
+        </thead>
+        <tbody class="px-5">
+            <tr>
+                <td class="px-5 col-6">Donnerstag + Freitag</td>
+                <td class="px-5 col-6"> 14:30 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Samstag + Sonntag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                Öffnungszeiten Ferien + Feiertage NRW
+            </th>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="px-5 col-6">Montags, Dienstags + Donnerstag bis Sonntag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6"><b>Am Mittwoch ist Ruhetag</b></tr>
+        </tbody>
+    </table>
+
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                Öffnungszeiten Ostern
+            </th>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="px-5 col-6">Karfreitag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Karsamstag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Ostersonntag</td>
+                <td class="px-5 col-6 text-danger">geschlossen</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Ostermontag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Osterferien (NRW) Außer Mittwochs</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6"><b>Am Mittwoch ist Ruhetag</b></tr>
+        </tbody>
+    </table>
+
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                Öffnungszeiten Weihnachten und Silvester</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="px-5 col-6"><b>Heiligabend</b></td>
+                <td class="px-5 col-6">geschlossen</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">1. Weihnachtsfeiertag</td>
+                <td class="px-5 col-6">geschlossen</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">2. Weihnachtsfeiertag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Silvester</td>
+                <td class="px-5 col-6">geschlossen</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Neuhjahr</td>
+                <td class="px-5 col-6">14:30 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Weihnachtsferien (NRW) Außer Mittwochs</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6"><b>Am Mittwoch ist Ruhetag</b></tr>
+        </tbody>
+    </table>
+
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                Sonderöffnungszeiten für Schulklassen ab 80 Kindern
+            </th>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="px-5 col-6">Donnerstag oder Freitag</td>
+                <td class="px-5 col-6">9:00 - 12:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6"><b>*Eine Anmeldung ist erforderlich</b></tr>
+        </tbody>
+    </table>
+
+    <table class="table">
+        <thead class="table-secondary">
+            <th class="bg-light col-6 text-center" colspan="2">
+                 <br>
+            </th>
+        </thead>
+    </table>
+
+    <p class="mx-5">
+    </p>
 </main>
 
 <?php include 'footer.php'; ?>
