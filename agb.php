8dfd1235fdbe3004f9fa524da908f8b7dba88cf5
laufwerkd <online.danielakinschin@gmail.com>
2022-11-11 10:37:24 +0100

    vorlage für seiten erstellt und agb seite erstellt und beendet

--- agb.php
+++ agb.php
@@ -8,4 +8,170 @@
     </div>
 </div>
 
+<div class="container-fluid">
+    <div class="container-lg py-5">
+        <h3>Regeln im Springolino (AGB)</h3>
+        <p>Die Regeln dienen vor allem der Sicherheit der spielenden Personen sowie dem Erhalt der Spielgeräte, dem entspannten miteinander und der Entlastung des Betreibers und seiner Mitarbeiter. Mit dem betreten der Spieleinrichtung werden die Regeln anerkannt.</p>
+        <p>Wir bitten Sie diese Regeln zu lesen, einzuhalten und auch Ihre Kinder entsprechend aufzuklären und anzuweisen.</p>
+        <p>Den Anweisungen des Betreibers und seiner Mitarbeiter ist unbedingt folge zu leisten!</p>
+        <p class="fst-italic">Vielen Dank! Ihr Springolino Team</p>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg2" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Allgemeines</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Die jeweils gültigen Preise und Öffnungszeiten sind im Aushang an der Kasse und unseren Informationsblättern zu entnehmen.</li>
+            <li class="list-group-item">Das Mitbringen von Tieren ist nicht gestattet</li>
+            <li class="list-group-item">Alle Anlagen und Einrichtungen der Halle dürfen nur entsprechend ihrer Zweckbestimmung benutzt werden.</li>
+            <li class="list-group-item">Die Nutzung sämtlicher Spielgeräte und Einrichtungen erfolgt auf eigene Gefahr, unbeschadet der Verpflichtung des Betreibers die Einrichtung in einem gebrauchsfähigen und sicheren Zustand zu erhalten. Der Betreiber und seine Mitarbeiter haften nur bei Vorsatz und Fahrlässigkeit.</li>
+            <li class="list-group-item">Filmen und fotografieren ist in unserer Halle erlaubt. Fremde Personen dürfen nur mit deren Zustimmung aufgenommen werden. Für gewerbliche Zwecke und für die Presse bedarf das Fotografieren einer vorherigen Genehmigung durch die Betreibergesellschaft.</li>
+            <li class="list-group-item">Kundendaten, die der Betreiber (z.B.) durch Geburtstagsanmeldungen erhält, werden gemäß Datenschutzgesetz behandelt, d.h. es erfolgt eine rein interne Verwendung ohne Weitergabe an Dritte.</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg1" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Keine Aufsichtspflicht / Haftungsfreistellung</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Die Aufsicht- und Betreuungspflicht obliegt einzig den Eltern bzw. den begleitenden Erwachsenen. Kindern ist der Besuch nur in Begleitung eines Aufsichtspflichtigen Erwachsenen gestattet, es sei denn, das Kind ist mindestens 12 Jahre alt und das entsprechende Einverständnis- und Haftungsfreistellungsformular wurde von einem Aufsichtspflichtigen unterschrieben und von dem jeweiligen Kind ein gültiger Lichtbildausweis vorgelegt.</li>
+            <li class="list-group-item">Der Betreiber und seine Mitarbeiter übernehmen in keinem Fall, auch nicht bei allein spielenden Kindern mit Haftungsfreistellung, eine Aufsicht –  und Betreuungspflicht. Dies gilt auch dann, wenn der Betreiber eine Sonderaktion für die Kinder durchführt (z.B. Karneval, etc.).</li>
+            <li class="list-group-item">Auch wenn an einzelnen Spielgeräten Aufsichtspersonal zur Verfügung gestellt wird, werden Eltern oder Aufsichtspflichtige Erwachsene, nicht von der eigenen Aufsichtspflicht entbunden.</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg2" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Benutzung der Spielgeräte</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Die an den Spielgeräten angebrachten Hinweise, Regeln und Verbote sind zu beachten! Die Benutzung erfolgt auf eigene Gefahr und erfordert Rücksichtnahme auf die Anderen Besucher. Die Begleitpersonen sind angehalten dem Kind / den Kindern die Spielregeln zu erläutern und auf ihre Einhaltung zu achten.</li>
+            <li class="list-group-item">Der Kleinkinderbereich ist nur für Krabbel- und Kleinkinder bestimmt. Störende Kinder über 5 Jahre dürfen vom Betreiber, seinen Mitarbeitern, sowie von Kleinkindermüttern aus dem Bereich verwiesen werden.</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg1" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Außenbereich</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Der Außenbereich darf nur durch die dafür vorgesehene Tür/Türen betreten werden, die Benutzung der Fluchttüren ist außerhalb eines Notfalls absolut verboten!</li>
+            <li class="list-group-item">Zur Sicherheit der eigenen und der anderen Kinder darf im gesamten Spielbereich kein eigenes Spielzeug benutzt werden. Vor allem ist es strikt untersagt, harte, lose oder spitze Gegenstände mit in den Spielbereich zu nehmen (dies gilt z.B. auch für neue Geburtstagsgeschenke).</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg2" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Videoüberwachung / Unfallmeldung</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Wir weisen darauf hin, dass der gesamte Spiel- und Gastronomiebereich videoüberwacht ist. Mit Nutzung der Räumlichkeiten wird diesem Vorgehen ausdrücklich zugestimmt.</li>
+            <li class="list-group-item">Sollte Ihnen etwas gestohlen worden sein, bitte unverzüglich an der Information melden.</li>
+            <li class="list-group-item">Wir zeigen jeden nachgewiesenen Diebstahl im Interesse unserer ehrlichen Kunden an und verhängen ein Hausverbot!</li>
+            <li class="list-group-item">Wichtig! Verletzungen, Personen- oder Sachschäden, gleich welcher Art, sind sofort an der Information zu melden und werden dort schriftlich dokumentiert. Meldungen nach Verlassen der Halle können aus versicherungstechnischen Gründen nicht mehr berücksichtigt werden. Sollte dies aus nachweisbaren Gründen nicht zumutbar gewesen sein, so ist die schriftliche Meldung unverzüglich, jedoch innerhalb von zwei Tagen unter Angabe von Datum und ungefährer Uhrzeit, nachzuholen.</li>
+            <li class="list-group-item">Für Unfälle, die nicht in dieser Form und Frist angezeigt werden, wird die Haftung ausgeschlossen.</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg1" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Bekleidung</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Aus hygienegründen sollten Socken, am besten Stoppersocken oder auch Gymnastikschläppchen getragen werden. Socken können Sie an der Information kaufen.</li>
+            <li class="list-group-item">Kinder oder spielende Erwachsene sollten bequeme Kleidung tragen. Bitte keine außenliegenden Reißverschlüsse oder Knöpfe, da damit die Folien der Spielgeräte beschädigt werden.</li>
+            <li class="list-group-item">Bitte Anhänger, Ketten, sowie hängende Ohrringe ablegen und bitte keine Kleidung mit Kordeln oder Bändchen tragen. Achtung sehr gefährlich!</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg2" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Speisen / Getränke / Rauchen / Alkohol</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Unsere Gastronomie bietet Ihnen ein reichhaltiges Angebot an Speisen und Getränken. Um die Preise fair und familiengerecht halten zu können, appellieren wir an Ihr Verständnis, dass wir das Mitbringen von Speisen und Getränken nicht gestatten können. Davon ausgenommen  sind Allergiker- und Babynahrung. Für Geburtstagsbuchungen gelten etwas andere Regeln, siehe dazu Geburtstags Info.</li>
+            <li class="list-group-item">Im Springolino gilt Kaugummiverbot!</li>
+            <li class="list-group-item">Essen und Trinken bitte nur an den dafür vorgesehen Tischen! Eine Mitnahme, auch von Glas oder Porzellan, in den Spielbereich ist streng untersagt!</li>
+            <li class="list-group-item">In der gesamten Halle, sowie auf den Toiletten, ist das Rauchen streng verboten und nur in dem dafür vorgesehen und gekennzeichneten Bereich erlaubt. Selbiges gilt auch für das anzünden von Kerzen, Wunderkerzen oder ähnlichem.</li>
+            <li class="list-group-item">Zuwiderhandeln kann mit Verweisung der Spielanlage führen.</li>
+            <li class="list-group-item">Liebe Gäste, bitte konsumieren Sie Alkohol nur in Maßen. Auffällig alkoholisierte Gäste werden aus der Halle verwiesen, zum Schutz der Kinder und Gäste!</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg1" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Verhalten / Beschädigungen</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Schlechtes Benehmen andere Kinder, z.B kratzen, beißen, schlagen, Schimpfwörter etc. wird nicht toleriert. Die aufsichtspflichtigen Eltern werden gebeten, diese Verhaltensweisen zu unterbinden. Dem Betreiber und seinen Mitarbeitern ist es ausdrücklich gestattet, Kinder in einem solchen Fall abzumahnen und im Wiederholungsfall Spielverbot zu erteilen. In einem solchen Fall wird der Eintrittspreis nicht erstattet. In besonders gravierenden Fällen, behält sich der Betreiber vor, einzelnen Personen Hausverbot zu erteilen.</li>
+            <li class="list-group-item">Bei Beschädigungen besteht seitens des Springolinos ein Schadenersatzanspruch, sofern Verschulden vorgeworfen werden kann. Wir möchten Sie bitten, uns diese Fälle anzuzeigen, damit wir sie mit der Haftpflichtversicherung Ihres Kindes abklären können.</li>
+            <li class="list-group-item">Die Aufsichtspflichtigen Eltern sind verpflichtet, Ihre Kinder von mutwilligen Zerstörungen abzuhalten. Im Falle des Verstoßes haften Sie dem Betreiber für den daraus entstandenen Schaden.</li>
+            <li class="list-group-item">Den Anweisungen des Personals ist unbedingt Folge zu leisten!</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg2" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Keinen Zutritt haben</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Erwachsene ohne Begleitung von Kindern (ausgenommen nachzügelnde und vorher angemeldete Erwachsene).</li>
+            <li class="list-group-item">Personen mit Hausverbot.</li>
+            <li class="list-group-item">Personen, deren Zutritt bedenklich erscheint (z.B. stark alkoholisiert).</li>
+            <li class="list-group-item">Besuchern mit ansteckenden Krankheiten oder offenen Wunden.</li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg1" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Salvatorische Klausel</h1>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-lg py-5 px-4">
+        <p>Sollten einzelne Regelungen unwirksam sein oder werden, so ist dadurch die Wirksamkeit der übrigen Regelungen nicht betroffen.</p>
+        <p>Zum Wohle eines sicheren Besuches und des zu beschützenden Kindes, bitten wir um Verständnis dieser Regelungen und wünschen viel Spaß in unserem größten Indoor- Spielplatz OWL`s!</p>
+        <p class="fst-italic">Ihr Springolino Team</p>
+    </div>
+</div>
+
 <?php include 'footer.php'; ?>
---
+++ vorlage.php
@@ -0,0 +1,31 @@
+<?php include 'header.php'; ?>
+
+<div class="container-fluid position-relative bg1" style="height: 33vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white fw-bold" style="font-family: marker;">
+            TITEL DER SEITE
+        </h1>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="container-lg py-5 text-center">
+        <h3 class="fw-bold">Überschrift</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore ex numquam in perspiciatis? Ab nulla vel qui voluptates, libero ea nihil molestiae, odio aperiam, eveniet in nam deserunt ut sunt!</p>
+    </div>
+</div>
+
+<div class="container-fluid position-relative bg2" style="height: 20vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white" style="font-family: marker;">Kleiner Titel</h1>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="container-lg py-5">
+        <h3 class="fw-bold">Noch mehr Inhalt</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore ex numquam in perspiciatis? Ab nulla vel qui voluptates, libero ea nihil molestiae, odio aperiam, eveniet in nam deserunt ut sunt!</p>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
