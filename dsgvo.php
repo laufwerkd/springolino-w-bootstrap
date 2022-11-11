170b92771003d1ec934d96df927ef58fb1d1e47e
LuluKlaschka <luisa.klaschka@edu.bib.de>
2022-11-11 09:54:29 +0100

    Update dsgvo.php
    Überschrift anders benannt

--- dsgvo.php
+++ dsgvo.php
@@ -3,7 +3,7 @@
 <div class="container-fluid position-relative bg1" style="height: 33vh">
     <div class="container-lg position-absolute top-50 start-50 translate-middle">
         <h1 class="text-center text-white fw-bold" style="font-family: marker;">
-            Datenschutz nach der DSGVO
+            Datenschutzerklärung nach der DSGVO(EU)
         </h1>
     </div>
 </div>
