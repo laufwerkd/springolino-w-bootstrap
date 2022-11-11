a139d91bee9ab81f1ac83df65d97e31af4ac2fbc
LuluKlaschka <luisa.klaschka@edu.bib.de>
2022-11-11 09:48:36 +0100

    Create dsgvo.php
    Format/Style soll gleich aussehen wie bei den AGBs

---
+++ dsgvo.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+<div class="container-fluid position-relative bg1" style="height: 33vh">
+    <div class="container-lg position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-center text-white fw-bold" style="font-family: marker;">
+            Datenschutz nach der DSGVO
+        </h1>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
