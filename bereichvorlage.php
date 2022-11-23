e9cbbe390226450d8dbabb877b3d1e99645c5aa8
LuluKlaschka <luisa.klaschka@edu.bib.de>
2022-11-23 11:45:24 +0100

    ADD: bereichvorlage.php, kontakt.php
    Erstmals befüllt mit dem Inhalt der vorlage.php

---
+++ bereichvorlage.php
@@ -0,0 +1,37 @@
+<?php include 'header.php'; ?>
+
+<div style="height: 20vh;"></div>
+
+<!--
+    Überschrift / Willkommens Text
+    nur 1x verwenden
+-->
+<div class="container-fluid bg-gray" id="scrollen"><br></div>
+<div class="container-lg p-5 bg-white rounded-bottom text-center">
+    <h3 class="fw-bold">Attraktion 01</h3>
+    <h5>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h5>
+</div>
+
+<!--
+    Modul
+    mehrmals verwendbar
+-->
+<div class="modul">
+    <div class="container-lg d-flex justify-content-center p-0 pt-5 bg2">
+        <h1 class="p-3 m-0 w-75 bg-black bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+            Modul Überschrift
+        </h1>
+    </div>
+    <div class="container-lg p-5 bg-white rounded">
+        <h5>Text mit Titel</h5>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo, laboriosam eligendi! Recusandae incidunt illum nisi dolor quasi, ab ea cum repudiandae facilis beatae, explicabo amet. Corrupti nemo quae saepe tempore?</p>
+
+        <h5>Noch ein Titel</h5>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo, laboriosam eligendi! Recusandae incidunt illum nisi dolor quasi, ab ea cum repudiandae facilis beatae, explicabo amet. Corrupti nemo quae saepe tempore?</p>
+
+        <h5>..und so weiter</h5>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo, laboriosam eligendi! Recusandae incidunt illum nisi dolor quasi, ab ea cum repudiandae facilis beatae, explicabo amet. Corrupti nemo quae saepe tempore?</p>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
