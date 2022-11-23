cc3300a93ed85cbcfcfd4f833be22007e9f51dae
H0unt <fabian.weber@edu.bib.de>
2022-11-23 11:47:07 +0100

    preiseZeiten Update

---
+++ preiseZeiten.php
@@ -0,0 +1,239 @@
+<?php include 'header.php'; ?>
+
+<div style="height: 20vh;"></div>
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-black bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        Preise & Öffnungszeiten
+    </h1>
+</div>
+
+<div class="container-lg py-5 bg-white rounded text-center">
+    <div class="row">
+        <div class="col">
+            <a href="#" type="button" class="btn btn-info" style="--bs-btn-padding-y: 4rem; --bs-btn-padding-x: 4rem; --bs-btn-font-size: 1.8rem;">
+                Eintrittspreise
+            </a>
+        </div>
+
+        <div class="col">
+            <a href="#" type="button" class="btn btn-primary" style="--bs-btn-padding-y: 4rem; --bs-btn-padding-x: 4rem; --bs-btn-font-size: 1.8rem;">
+                Öffnungszeiten
+            </a>
+        </div>
+    </div>
+</div>
+
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-info bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        Tageseintritt
+    </h1>
+</div>
+
+<div class="container-lg p-4 bg-white rounded">
+    <table class="table">
+        <tbody class="px-5 bg-white">
+            <tr>
+                <td class="px-5 col-6">Kinder ab 3 Jahre</td>
+                <td class="px-5 col-6">11,50 € (unter 3 Jahre frei)</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Erwachsene ab 18 Jahre</td>
+                <td class="px-5 col-6">5,50 €</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Kinder mit Behindertenausweis</td>
+                <td class="px-5 col-6">6,00 € (unter 3 Jahre frei)</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Erwachsene mit Behindertenausweis</td>
+                <td class="px-5 col-6">3,00 € (unter 3 Jahre frei)</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-info bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        10er Karte
+    </h1>
+</div>
+
+<div class="container-lg p-4 bg-white rounded">
+    <table class="table">
+        <tbody class="bg-white">
+            <tr>
+                <td class="px-5 col-6">10er Kinder</td>
+                <td class="px-5 col-6">99,00 €</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">10er Erwachsene</td>
+                <td class="px-5 col-6">49,00 €</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-info bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        Information
+    </h1>
+</div>
+
+<div class="container-lg p-4 bg-white rounded">
+    <p class="px-5 bg-white">
+        <b>*Bei uns ist das Mitbringen von eigenen Speisen und Getränken nicht gestattet!
+        Wir bitten um Ihr Verständnis.</b> *Kinder unter 12 Jahren haben nur in Begleitung
+        eines Erwachsenen Zutritt. *Kinder über 12 Jahren haben gegen Vorlage eines
+        gültigen Personalausweises und nur mit Unterschrift eines Erziehungsberechtigten
+        Zutritt. Hier können Sie unsere Einverständniserklärung <a href="#">downloaden</a> Falls Sie Interesse
+        an einer Gruppenermäßigung haben, können Sie sich telefonisch unter 0 52 21 – 69 24 24 melden.
+    </p>
+</div>
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-primary bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        Öffnungszeiten generell
+    </h1>
+</div>
+
+<div class="container-lg p-4 bg-white rounded">
+    <table class="table">
+        <tbody class="px-5 bg-white">
+            <tr>
+                <td class="px-5 col-6">Donnerstag + Freitag</td>
+                <td class="px-5 col-6">14:30 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Samstag + Sonntag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-primary bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        Öffnungszeiten Ferien + Feiertage NRW
+    </h1>
+</div>
+
+<div class="container-lg p-4 bg-white rounded">
+    <table class="table">
+        <tbody class="bg-white">
+            <tr>
+                <td class="px-5 col-6">Montags, Dienstags + Donnerstag bis Sonntag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6"><b>Am Mittwoch ist Ruhetag</b></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-primary bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        Öffnungszeiten Ostern
+    </h1>
+</div>
+
+<div class="container-lg p-4 bg-white rounded">
+    <table class="table">
+        <tbody class="bg-white">
+            <tr>
+                <td class="px-5 col-6">Karfreitag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Karsamstag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Ostersonntag</td>
+                <td class="px-5 col-6 text-danger">geschlossen</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Ostermontag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Osterferien (NRW) Außer Mittwochs</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6"><b>Am Mittwoch ist Ruhetag</b></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-primary bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        Öffnungszeiten Weihnachten / Silvester
+    </h1>
+</div>
+
+<div class="container-lg p-4 bg-white rounded">
+    <table class="table">
+        <tbody class="bg-white">
+            <tr>
+                <td class="px-5 col-6"><b>Heiligabend</b></td>
+                <td class="px-5 col-6 text-danger"><b>geschlossen</b></td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">1. Weihnachts - feiertag</td>
+                <td class="px-5 col-6 text-danger">geschlossen</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">2. Weihnachts - feiertag</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Silvester</td>
+                <td class="px-5 col-6 text-danger">geschlossen</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Neuhjahr</td>
+                <td class="px-5 col-6">14:30 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6">Weihnachts - ferien (NRW) Außer Mittwochs</td>
+                <td class="px-5 col-6">11:00 - 19:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6"><b>Am Mittwoch ist Ruhetag</b></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<div class="container-lg d-flex justify-content-center p-0 pt-5">
+    <h1 class="p-3 m-0 w-50 bg-primary bg-opacity-25 text-center text-white rounded-top" style="font-family: marker; backdrop-filter: blur(5px);">
+        Sonder Öffnungszeiten für Schulklassen ab 80 Kindern
+    </h1>
+</div>
+
+<div class="container-lg p-4 mb-5 bg-white rounded">
+    <table class="table">
+       
+        <tbody class="bg-white">
+            <tr>
+                <td class="px-5 col-6">Donnerstag oder Freitag</td>
+                <td class="px-5 col-6">9:00 - 12:00 Uhr</td>
+            </tr>
+            <tr>
+                <td class="px-5 col-6"><b>*Eine Anmeldung ist erforderlich</b></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
