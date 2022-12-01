25cd41b86e15e873c473f10e2c5934f89ed14d61
LuluKlaschka <luisa.klaschka@edu.bib.de>
2022-12-01 15:55:29 +0100

    Update kontakt.php
    Kontaktdaten hinzugefügt und das Formular überarbeitet. Ein bisschen Styles hinzugefügt, berbeitungspflichtig

--- kontakt.php
+++ kontakt.php
@@ -13,18 +13,34 @@
             Kontakt
         </h1>
     </div>
-    <div class="container-lg p-4 bg-white rounded-top">
+    <div class="p-3 px-4 m-0 bg-primary bg-opacity-25 text-center text-white rounded-top" style="max-width: 75%; font-family: marker; backdrop-filter: blur(5px);">
         <h5>Unsere Kontaktdaten</h5>
-        <form method="post" autocomplete="off">
-            <label for="name">Name</label>
-            <input id="name" name="name" maxlength="50">
-            <label for="mail">E-Mail</label>
-            <input id="mail" name="email" maxlength="50">
-            <input type="checkbox" name="datenschutz" id="dsgvo" value="akzeptieren">
-            <label for="dsgvo">Ich habe die AGBs und die Datenschutzbestimmungen gelesen und akzeptiert!</label>
-            <button class="btn btn-primary" type="submit">Senden</button>
-            <button class="btn btn-danger" type="reset">Eingaben löschen</button>
-        </form>
+        <p>Du hast Fragen, Anregungen oder möchtest einfach mit uns in Kontakt treten?
+            Kein Problem! Hier findest du alles was du dazu benötigst:</p>
+            <p>Springolino GmbH<br>
+                Ackerstraße 35<br>
+                32051 Herford<br>
+                Telefon : 0 52 21 – 69 24 24<br>
+                Telefax : 0 52 21 – 69 24 25<br>
+                E-Mail : fun@springolino.de</p><br>
+    </div>   
+</div>
+<div class="modul">
+    <div class="container-lg d-flex justify-content-center p-0 pt-5">
+        <div class="p-3 px-4 m-0 bg-primary bg-opacity-25 text-center text-white rounded-top" style="max-width: 75%; font-family: marker; backdrop-filter: blur(5px);">
+            <form method="post" autocomplete="off">
+                <label for="name">Name</label>
+                <input id="name" name="name" maxlength="50" placeholder="Dein Vor- und Nachname..."><br>
+                <label for="mail">E-Mail</label>
+                <input id="mail" name="email" maxlength="50" placeholder="z.B. max.mustermann@mustermail.de"><br>
+                <label for="msg">Ihre Nachricht:</label>
+                <input id="msg" name="msg" minlength="10" placeholder="Nachricht..."><br>
+                <input type="checkbox" name="datenschutz" id="dsgvo" value="akzeptieren">
+                <label for="dsgvo">Ich habe die AGBs und die Datenschutzbestimmungen gelesen und akzeptiert!</label><br>
+                <button class="btn btn-primary" type="submit">Senden</button>
+                <button class="btn btn-danger" type="reset">Eingaben löschen</button>
+            </form>
+        </div>
     </div>
 </div>
 
