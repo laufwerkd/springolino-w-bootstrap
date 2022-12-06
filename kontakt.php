675e9bda46c77b73cc6297e516890597c7fcc059
LuluKlaschka <luisa.klaschka@edu.bib.de>
2022-12-06 14:43:34 +0100

    Update kontakt.php
    Style änderung

--- kontakt.php
+++ kontakt.php
@@ -6,7 +6,7 @@
         background-image: url(img/bg/bg1.jpg);
     }
 
-    input[type=text], select, textarea {
+   /* input[type=text], select, textarea {
         width: 100%;
         padding: 12px;
         border: 1px solid rgb(255, 255, 255);
@@ -15,18 +15,18 @@
         margin-top: 6px;
         margin-bottom: 16px;
         resize: vertical;
-    }
+    }*/
 </style>
 
 <div class="modul">
     <div class="container-lg d-flex justify-content-center p-0 pt-5">
-        <h1 class="p-3 px-4 m-0 bg-info bg-opacity-25 text-center text-white rounded-top" style="max-width: 75%; font-family: marker; backdrop-filter: blur(5px);">
+        <h1 class="p-3 px-4 m-0 bg-primary bg-opacity-25 text-center text-white rounded-top" style="max-width: 75%; font-family: marker; backdrop-filter: blur(5px);">
             Kontakt
         </h1>
     </div>
-    <div class="p-3 px-4 m-0 bg-primary bg-opacity-25 text-center text-white rounded-top" style="max-width: 75%; font-family: marker; backdrop-filter: blur(5px);">
+    <div class="p-3 px-4 m-0 bg-secondary bg-opacity-75 text-center text-white rounded" style="max-width: 75%; font-family: marker; backdrop-filter: blur(7.5px);">
         <h5>Unsere Kontaktdaten</h5>
-        <p>Du hast Fragen, Anregungen oder möchtest einfach mit uns in Kontakt treten?
+        <p>Du hast Fragen, Anregungen oder möchtest einfach mit uns in Kontakt treten?<br>
             Kein Problem! Hier findest du alles was du dazu benötigst:</p>
             <p>Springolino GmbH<br>
                 Ackerstraße 35<br>
@@ -38,14 +38,14 @@
 </div>
 <div class="modul">
     <div class="container-lg d-flex justify-content-center p-0 pt-5">
-        <div class="p-3 px-4 m-0 bg-primary bg-opacity-25 text-center text-white rounded-top" style="max-width: 75%; font-family: marker; backdrop-filter: blur(5px);">
+        <div class="p-3 px-4 m-0 bg-secondary bg-opacity-75 text-center text-white rounded" style="max-width: 60%; font-family: marker; backdrop-filter: blur(7.5px);">
             <form method="post" autocomplete="off">
                 <label for="name">Name</label>
                 <input id="name" name="name" maxlength="50" placeholder="Dein Vor- und Nachname..."><br>
                 <label for="mail">E-Mail</label>
                 <input id="mail" name="email" maxlength="50" placeholder="z.B. max.mustermann@mustermail.de"><br>
                 <label for="msg">Ihre Nachricht:</label>
-                <textarea id="msg" name="msg" minlength="10" placeholder="Nachricht..." style="height:200px"><br>
+                <textarea id="msg" name="msg" minlength="10" placeholder="Nachricht..." style="height:200px"></textarea><br>
                 <input type="checkbox" name="datenschutz" id="dsgvo" value="akzeptieren">
                 <label for="dsgvo">Ich habe die AGBs und die Datenschutzbestimmungen gelesen und akzeptiert!</label><br>
                 <button class="btn btn-primary" type="submit">Senden</button>
